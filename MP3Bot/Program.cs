ee8bcef31177a3f47b4df9e6dfcc399e1dd03c3c
vittka1105 <107803187+vittka1105@users.noreply.github.com>
2022-06-29 23:49:45 +0300

    Добавьте файлы проекта.

---
+++ MP3Bot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MP3Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MP3bot mp3Bot = new MP3bot();
+            mp3Bot.Start();
+            Console.ReadKey();
+        }
+    }
+}
